262de8b0dac77d8a702011270c5f2429a11d70b0
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-03-25 22:20:12 +0530

    Allusers and Singleuser file created

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React, {createContext, useState} from "react";
 import "./style.css";
 import Child from './Child';
 import Data from './Data';
+import AllUsers from './pages/AllUsers';
 
 export const GlobalContext = createContext();
 
-export default function App() {
+ const App = ()=> {
   const [color, setColor] = useState('red');
-
 
   return (
     <GlobalContext.Provider value={{appColor:color}}>
       <h1>App.js</h1>      
       <Child />
       <Data />
+      <AllUsers />
     </GlobalContext.Provider>
   );
 }
+
+export default App;
---
+++ src/pages/AllUsers.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const AllUsers = () => {
+  return (
+    <section className="allusers">
+      <div className="container">
+        <div className="users">
+          <h4>Select an account</h4>
+          <div className="users-list">
+            {/* <UserList users={props.users} /> */}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+
+// const AllUsers = () => {
+
+//   return (
+//     <h1>AllUsers.js</h1>
+//   );
+// };
+
+export default AllUsers;
