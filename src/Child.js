b76c4b2a14bbd0b38647bcc1003ca5e3128b32b5
CHANDRASHEKHAR MESHRAM <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-03-25 20:47:10 +0530

    Context used

--- src/Child.js
+++ src/Child.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {GlobalContext} from './App';
 
 const Child = ()=> {
-
+  const {appColor} = useContext(GlobalContext);
+  console.log("appColor - ", appColor);
   return(
-    <h1>Child.js</h1>
+    <h1 style={{color: appColor}}>Child.js</h1>
   )
 }
 
